1dbbb9796f8a3ea389a5983172fce0f0deb0e44b
JanPluhar <honza.shift@outlook.com>
2024-10-19 21:07:32 +0200

    Up

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,7 +1,39 @@
+const User = require("../models/user");
+
 const test = (req, res) => {
   res.json("Route via test is working correctly");
 };
 
+const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    // Check if name was entered
+    if (!name) {
+      return res.json({ error: "Name is required" });
+    }
+    // Check if password is good
+    if (!password || password.length < 6) {
+      return res.json({
+        error: "Password is required and must be 6 characters or longer",
+      });
+    }
+    // Check if email is good
+    // const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const exist = await User.findOne({ email });
+    if (exist) {
+      return res.json({ error: "Email already exists" });
+    }
+
+    const user = await User.create({ name, email, password });
+
+    return res.json(user);
+  } catch (error) {
+    console.log(error);
+    return res.json({ error: "Something went wrong" });
+  }
+};
+
 module.exports = {
   test,
+  registerUser,
 };
--- server/index.js
+++ server/index.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const cors = require("cors");
+const Mongoose = require("mongoose");
 
 const app = express();
 
+// Connect to MongoDB
+Mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((error) => console.log("Error connecting to MongoDB", error));
+
+// Middleware
+app.use(express.json());
+
 app.use("/", require("./routes/authRoutes"));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
